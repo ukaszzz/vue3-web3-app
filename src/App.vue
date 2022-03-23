06d9d20275efff3ba635fb4b12f5df686cc6e6ad
Lukasz Zola <zolalukas@gmail.com>
2022-03-23 20:56:46 +0100

    add store, loader and index

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
+import { useCryptoStore } from '~/stores/crypto'
+const props = useCryptoStore()
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
-useHead({
-  title: 'Vitesse',
-  meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
-  ],
-})
+// they will be rendered correctly in the html results with vite-ss
 </script>
 
 <template>
+  <Loader v-if="props.loading" />
   <router-view />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
-import './styles/main.css'
 import 'uno.css'
 
 const routes = setupLayouts(generatedRoutes)
